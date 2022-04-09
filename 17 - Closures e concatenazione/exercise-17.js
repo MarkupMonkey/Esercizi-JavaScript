89504b4d58eb26b2afead5c1f50f36097335f68a
Marco Moncada <marco.monkey93@gmail.com>
2022-04-09 12:06:49 +0200

    refactored exercise 17

--- 17 - Closures e concatenazione/exercise-17.js
+++ 17 - Closures e concatenazione/exercise-17.js
@@ -1,32 +1,13 @@
 function calculate() {
-  return new Object({
-  tot:0,
-    add(number){
-      this.tot += number;
-      return this;
-    },
-
-    multiply(number){
-      this.tot *= number;
-      return this;
-    },
-
-    sub(number){
-      this.tot -= number;
-      return this;
-    },
-
-    divide(number){
-      this.tot /= number;
-      return this;
-    },
-
-    printResult(){
-      return this
-    },
-    
-  });
+  tot = 0;
+  return total = { 
+    add(number){ tot = tot + number; return this},
+    multiply(number){ tot = tot * number; return this},
+    sub(number){ tot = tot - number; return this},
+    divide(number){ tot = tot / number; return this},
+    printResult(){ console.log(tot)}
+  }
 }
 const calculator = calculate();
 
-console.log(calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult()); // Il risultato sarà: 7
+calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
