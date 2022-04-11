a04ef72957830c5da7011bec35f37c07efaa7878
Marco Moncada <marco.monkey93@gmail.com>
2022-04-11 10:42:02 +0200

    Object reference ex 1 done

--- 22 - Object references/exercise-22.js
+++ 22 - Object references/exercise-22.js
@@ -3,8 +3,13 @@ const person = {
   lastName: 'Doe',
   age: 25,
   job: 'Web Developer',
-  // ...
-}
+    fullName(){
+    return `${this.firstName}, ${this.lastName}`;
+    },
+    info(){
+    return `${this.age}, ${this.job}`; 
+    }
+} 
 
 console.log(person.fullName()); // John Doe
 console.log(person.info()); // John Doe, 25 - Web Developer
