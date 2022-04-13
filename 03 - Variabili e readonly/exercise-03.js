2843dd5d20de3b6dd90e2acb32e5615fa599a03a
Marco Moncada <marco.monkey93@gmail.com>
2022-04-13 14:42:30 +0200

    fixed exercise 3

--- 03 - Variabili e readonly/exercise-03.js
+++ 03 - Variabili e readonly/exercise-03.js
@@ -1,8 +1,11 @@
 const students = ['Paul', 'George', 'Lucas'];
-
+// console.log(`---- befor push ----`)
 function addStudent(student) {
-  students.push('Marco');
+  console.log(students);
+  // console.log(`--- after push ---`)
+  students.push(student);
 }
-
+//addStudent('Luigi');
 addStudent('Marco');
+//addStudent('Gianfrancioschio');
 console.log(students);
