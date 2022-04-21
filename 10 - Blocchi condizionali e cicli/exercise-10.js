b74390a1ee3df4c5f20e7f3f9285667bd65b5ac1
Marco Moncada <marco.monkey93@gmail.com>
2022-04-21 20:41:41 +0200

    fixed: exercise 10

--- 10 - Blocchi condizionali e cicli/exercise-10.js
+++ 10 - Blocchi condizionali e cicli/exercise-10.js
@@ -1,10 +1,10 @@
 let i = 0;
 function sumUntil(maxValue) {
-  let sum = 0;
-  let preSum = '';
-  for (let i = 1; i<= (maxValue); i++ ) {
+  let sum = maxValue;
+  let preSum = 1;
+  for (let i = 1; i<= (maxValue -1); i++ ) {
       sum = sum + i;
-      preSum = preSum + ' + ' + i;
+      preSum = preSum + ' + ' + (i + 1);
   }
     return `${preSum} = ${sum}`
 }
