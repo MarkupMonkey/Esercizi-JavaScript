ed54725b882047b7362cd587a75066b74b076db2
Marco Moncada <marco.monkey93@gmail.com>
2022-05-01 19:35:35 +0200

    Exercises 54 55 56 done

--- 54 - Rest parameters and spread syntax/exercise-54.js
+++ 54 - Rest parameters and spread syntax/exercise-54.js
@@ -5,11 +5,12 @@ const person = {
   age: 25
 };
 
-const id = person.id;
-const personInfo = {
-  firstName: person.firstName,
-  lastName: person.lastName,
-  age: person.age
-};
+const {id, ...personInfo} = {...person};
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
+//let { id, ...person } = personInfo;
 
 console.log(id, personInfo);
