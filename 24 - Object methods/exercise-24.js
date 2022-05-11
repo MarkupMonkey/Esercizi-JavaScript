3e854845801ca6ebd4daab8f8c1ecb94ccdcd130
Marco Moncada <marco.monkey93@gmail.com>
2022-05-11 14:45:04 +0200

    exercises 75 -76  done

--- 24 - Object methods/exercise-24.js
+++ 24 - Object methods/exercise-24.js
@@ -3,5 +3,8 @@ const person = {
   lastName: 'Rossi',
   age: 25
 }
-console.log(Object.values(person));
+//console.log(Object.values(person)); //['Mario', 'Rossi', 25]
+  for (const value of Object.values(person)) {
+    console.log(`${value}`);
+  }
 // Print values of person using Object.values
